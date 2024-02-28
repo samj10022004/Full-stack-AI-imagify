6f47bfa174ce9a582f1bea78f4de8b6076bb3f69
samj10022004 <97349711+samj10022004@users.noreply.github.com>
2024-02-28 18:46:26 +0530

    done

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '@/components/shared/Header'
+import Header from '@/components/shared/header'
 import { transformationTypes } from '../../../../../../constants'
 import { title } from 'process';
 import TransformationForm from '@/components/shared/TransformationForm';
